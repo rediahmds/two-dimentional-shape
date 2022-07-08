fe87246b8efb72c4f3a1ba7c9bdb6429ee2927cd
Redi Ahmad Supriyatna <ahmadredi67@gmail.com>
2022-07-09 00:23:09 +0700

    Give some comment for several lines of code

--- right-triangle.py
+++ right-triangle.py
@@ -1,9 +1,12 @@
 print(15*'+', 'SEGITIGA SIKU SIKU', 15*'*')
 print(8*'+', 'FOR LOOP', 8*'*')
+
+# define how much the iteration occur
 batas = int(input('Berapa kali loop? '))
 for i in range(1, batas + 1):
     print(i*'*')
 
+# give some space in between
 print()
 print()
 
